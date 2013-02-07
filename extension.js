c99bb8134dffc5bc0d7f4abeb80fa225c59bb423
fortime <palfortime@gmail.com>
2013-02-07 08:44:11 +0800

    add a icon to panel

--- extension.js
+++ extension.js
@@ -1,53 +1,54 @@
-
+const GLib = imports.gi.GLib;
+const Gdk = imports.gi.Gdk;
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
 const St = imports.gi.St;
+
 const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
 const Tweener = imports.ui.tweener;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+const ThisExtension = ExtensionUtils.getCurrentExtension();
 
 let text, button;
+let indicator;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
+SSHDProxyIndicator.prototype = {
+    __proto__: PanelMenu.Button.prototype,
+
+    _init: function() {
+		PanelMenu.Button.prototype._init.call(this, St.Align.START);
+	    // __proto__._init.call(this, St.Align.START);
+        
+        // add our icons path for searching
+        Gtk.IconTheme.get_default().append_search_path(ThisExtension.dir.get_child('icons').get_path());
+
+        this.proxyIcon = new St.Icon({icon_name: "proxy-enabled-symbolic", 
+            style_class: 'system-status-icon'});
+        this.layoutManager = new St.BoxLayout({vertical: false, 
+            style_class: 'ssdproxy-container'});
+        this.layoutManager.add(this.proxyIcon);
+        this.actor.add_actor(this.layoutManager);
+        this.proxyIcon.show();
+    }
 }
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
-                      Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+function SSHDProxyIndicator() {
+    this._init();
 }
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
-
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    indicator = new SSHDProxyIndicator(); 
+    Main.panel.addToStatusArea('sshdproxy', indicator, 2);
+    //Main.panel._rightBox.insert_child_at_index(button, 0);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    //Main.panel._rightBox.remove_child(button);
+    indicator.destroy();
 }
